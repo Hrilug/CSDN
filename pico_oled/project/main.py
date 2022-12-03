ddc566fdf076e887b9b08c26988a5953c6d21e3b
Hrilug <3198727736@qq.com>
2022-12-03 23:10:42 +0800

    2022-12-3-1

--- pico_oled/project/main.py
+++ pico_oled/project/main.py
@@ -1,6 +1,6 @@
 from ssd1306 import SSD1306_I2C
 from machine import I2C,Pin
-import time,machine
+import time,machine,framebuf
 
 i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=2000000) 
 display = SSD1306_I2C(128,64,i2c)
